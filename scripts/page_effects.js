61f8f6a3bb56577863a444047b475872de4aa960
tnd23 <thomas.nast23@gmail.com>
2017-04-04 21:06:00 -0700

    Initial commit

---
+++ scripts/page_effects.js
@@ -0,0 +1,21 @@
+function clearContainer(){
+
+}
+
+// call function, get results, append to container
+function addContentToContainer(fnc){
+
+}
+
+function addStyle(target){
+
+}
+
+function removeStyle(){
+
+}
+
+function testJquery(){
+	alert("Test");
+
+}
---
+++ scripts/user_actions.js
@@ -0,0 +1,9 @@
+/*
+	Each user action corresponds with a behavior the user can take.
+	User actions may result in a change in the display of the page.
+*/
+function clickButton(btn){
+
+	// call button depending on which id is passed thru
+}
+
